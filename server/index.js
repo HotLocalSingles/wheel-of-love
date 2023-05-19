aeef089a063433b7f6b8715836a185ffa3b5833e
Cynthia <ladypenguin1991@gmail.com>
2023-05-19 18:34:13 -0500

    added chatboxsender and chatboxreciever components. Socket.io connected to server and can send messages outside of chatbox

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: '*' } });
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile('/home/useradd/Senior/wheel-of-love/Cynthia/index.html');
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log('listening on localhost:3000');
+});
+
+//building socket.io logic
+//event emitter to check for connection
+io.on('connection', (socket) => {
+  //socket event creation
+  console.log('socket id', socket.id);
+  //socket method
+  socket.on('message', (data) => {
+    console.log(data);
+    //emit data to everyone but self
+    // socket.emit(data);
+    socket.broadcast.emit('message', data);
+  });
 });
