59b92fe7589fe7847df7cde49b9bee730995a212
logan <loganh42@gmail.com>
2023-05-19 14:42:26 -0500

    Add Passport and Google Auth to Server Index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,47 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+//Including google auth's client id from the .env file here so that it can be used for the GoogleStrategy
+require('dotenv').config();
+
+
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const server = require('http').createServer(app);
+
+// Passport is being configured with the google auth information, also known as google strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+
+      //INSIDE OF HERE WE CALL THE USER INFORMATION, SEEING IF THEY LOGIN
+      //NEED TO SETUP WITH THE MYSQL DATABASE
+
+      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
+
+      return done(null, profile);
+    }
+  )
+);
+
+//Initialize Passport in the server
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+//Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+//Follow link for passport explanation: https://www.passportjs.org/packages/passport-google-oauth20/
