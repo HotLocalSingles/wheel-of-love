b68d37f97bf776ff7b754d721ae3f51602ddb3a5
logan <loganh42@gmail.com>
2023-05-20 18:31:26 -0500

    Add User Profile Component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,9 +3,10 @@ import axios from 'axios';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 
 //Material UI:
-import { Button, Avatar } from '@mui/material';
+import { Button } from '@mui/material';
 
 import Wheel from '../components/Wheel.jsx';
+import UserProfile from './UserProfile.jsx';
 
 const App = () => {
 
@@ -17,17 +18,15 @@ const App = () => {
   };
 
   /////////////////LOGAN LOGIN CODE/////////
-
-  //user is the logged in user
-  //profile is the user profile data from Google
-  const [user, setUser] = useState(null);
-  const [profile, setProfile] = useState(null);
-
   //useGoogleLogin hook gives the login functionality
   const login = useGoogleLogin({
     onSuccess: (codeResponse) => setUser(codeResponse),
     onError: (error) => console.log('Login Failed:', error)
   });
+
+  //profile is the user profile data from Google
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   //Hook that is used to fetch user profile data when the 'user' state is changed
   //Calling an axios get request
@@ -53,6 +52,7 @@ const App = () => {
     setProfile(null);
   };
 
+
   //Conditional statement for profile, when there is no profile, that means no one is logged in and it shows the login button
   //If the user is logged in, they see the 'Gettin' Around' Info, created by Cynthia in the original code
   //I added a div below it to display the google profile info to confirm that it had worked but we can edit it when needed
@@ -60,9 +60,9 @@ const App = () => {
     <div>
       {profile ? (
         <div>
+          <UserProfile profile={ profile }/>
           <div>
-            <h1>Gettin' Around</h1>
-            <h2>A Dating Site</h2>
+            <h1>Wheelin' Around</h1>
             <Wheel onUserSelected={handleUserSelected} />
             {selectedUser && (
               <div>
@@ -71,17 +71,9 @@ const App = () => {
               </div>
             )}
           </div>
-          <Avatar
-            alt="User Profile Image"
-            src={ profile.picture }
-            sx={{ width: 80, height: 80 }}
-          />
-          <h3>User Logged in</h3>
-          <p>Name: { profile.name }</p>
-          <p>Email Address: { profile.email }</p>
           <br />
           <br />
-          <button onClick={ logOut }>Log out</button>
+          <Button variant="contained" size="small" onClick={ logOut }>Log out</Button>
         </div>
       ) : (
         <div>
---
+++ client/src/components/UserProfile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// Material UI:
+import { Avatar} from '@mui/material';
+
+const UserProfile = ({ profile }) => {
+//user is the logged in user
+
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <Avatar
+        alt="User Profile Image"
+        src={ profile.picture }
+        sx={{ width: 80, height: 80 }}
+      />
+      <p>Name: { profile.name }</p>
+      <p>Email Address: { profile.email }</p>
+    </div>
+  );
+};
+
+export default UserProfile;
