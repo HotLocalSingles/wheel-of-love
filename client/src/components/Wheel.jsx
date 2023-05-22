b9724e3b35e13f954bff44d7815f775ec46ec6d6
Alex <xanderlambert@gmail.com>
2023-05-22 14:26:03 -0500

    update index.jsx to react18 createRoot, placed users around wheel

--- client/src/components/Wheel.jsx
+++ client/src/components/Wheel.jsx
@@ -21,16 +21,52 @@ const Wheel = ({ onUserSelected }) => {
   };
 
   return (
-    
-    <div style={{width: "50%", margin: "auto", textAlign: 'center'}}>
-       {/* This is the actual wheel div */}
-      <div style={{backgroundColor: "lightblue", borderRadius: "50%",  aspectRatio : "1 / 1", transform: `rotate(${rotationAngle}deg)`, transition: 'transform 1s',}}>
-      {users.map((user) => (
-        <div style={{color: "lightyellow"}}>{user}</div>      
-      ))}
+    <div style={{ width: '50%', margin: 'auto', textAlign: 'center' }}>
+      <div
+        style={{
+          // Wheel Container
+          backgroundColor: "lightblue",
+          position: 'relative',
+          width: '100%',
+          height: '0',
+          paddingBottom: '100%',
+          overflow: 'hidden',
+        }}
+      >
+        <div
+          style={{
+            // Actual wheel
+            backgroundColor: "lightgreen",
+            borderRadius: "50%",
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+            transform: `rotate(${rotationAngle}deg)`,
+            transition: 'transform 1s',
+          }}
+        >
+          {users.map((user, index) => (
+            //User Divs on wheel
+            
+            <div
+              key={index}
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: `translate(-50%, -50%) rotate(${index * (360 / users.length)}deg)`,
+                transformOrigin: 'center center',
+                color: 'lightyellow',
+              }}
+            >
+              {user}
+            </div>
+          ))}
+        </div>
       </div>
       <h3>Have Fate Pick your Date</h3>
-       {/* Display the spin button and the selected user */}
       {selectedUser ? (
         <div>
           <button onClick={spinWheel}>Spin Again</button>
@@ -40,7 +76,6 @@ const Wheel = ({ onUserSelected }) => {
         <div>
           <button onClick={spinWheel}>Spin the Wheel</button>
         </div>
-        
       )}
     </div>
   );
