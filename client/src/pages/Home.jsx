248c5f78aad2e84d7c975c711401fbb18cae28d6
Bryan Burnside <bpburnside@gmail.com>
2023-05-24 12:42:41 -0500

    Update vibe, icebraker; integrate into app and home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import UserProfile from '../components/UserProfile.jsx';
-
+import Icebreaker from '../components/Icebreaker.jsx';
 import Vibe from '../components/Vibe.jsx';
 
 import Wheel from '../components/Wheel.jsx';
 
 const Home = ({ user, handleLogout }) => {
-
   //The handleLogout is referring to the function in App.jsx and is changing the state in there
   //Button is from the material ui
 
@@ -16,6 +15,7 @@ const Home = ({ user, handleLogout }) => {
       <h1>Home</h1>
       <UserProfile user={ user }/>
       <Vibe />
+      <Icebreaker user={ user } />
       <Button variant="outlined" color="error" size="medium" onClick={ handleLogout }>Logout</Button>
 
       <br/>
