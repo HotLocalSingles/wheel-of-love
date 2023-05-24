d97bed931de771e853f773af4153691074dd6bf4
Bryan Burnside <bpburnside@gmail.com>
2023-05-24 13:49:21 -0500

    Create vibe route

---
+++ server/routes/vibeRoute.js
@@ -0,0 +1,20 @@
+//Express Requirements
+const express = require('express');
+const router = express.Router();
+
+//Importing Axios helper functions for icebreaker API
+const { getVibe } = require('../helpers/vibe.js');
+
+// Post bio to API for vibe check
+router.post('/api/vibe', async (req, res) => {
+  const { bio } = req.body;
+  try {
+    const response = await getVibe(bio);
+    res.status(201).send(response.data);
+  } catch (err) {
+    console.error('Failed to POST vibe from API');
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
