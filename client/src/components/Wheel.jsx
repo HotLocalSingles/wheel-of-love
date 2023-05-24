f9d50133ef5bd510cb6f320cb96cc81c455b561f
Alex <xanderlambert@gmail.com>
2023-05-24 17:58:00 -0500

    add checkboxes for filter

--- client/src/components/Wheel.jsx
+++ client/src/components/Wheel.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
-
 import Chat from '../components/Chat.jsx';
+import { FormControlLabel, Checkbox, Button } from '@mui/material';
 
 const Wheel = ({ user }) => {
   // State for the list of users, selected user, rotation angle
@@ -22,24 +22,22 @@ const Wheel = ({ user }) => {
       if (!insertUsers) {
         throw new Error(insertUsers);
       }
-      console.log("Backend call for all users:", insertUsers)
+      console.log('Backend call for all users:', insertUsers);
       setUsers(insertUsers);
     } catch (error) {
       console.error('Error fetching all users on client side wheel:', error);
     }
   };
- 
-  
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  //Getting the length of the array so it can create the position values
+  // This keeps the (positional) reference array and users array the same length,
+  // In case a user is added or removed in the future.
   useEffect(() => {
     userRefs.current = userRefs.current.slice(0, users.length);
   }, [users.length]);
-
 
   const spinWheel = () => {
     // Calculate the rotation increment and update the rotation angle
@@ -56,29 +54,32 @@ const Wheel = ({ user }) => {
 
     // After the rotation duration, set the selected user and invoke the callback
     setTimeout(() => {
-    // Get the y-coordinate of each user div. rect is for rectangle
-      const userYCoordinates = userRefs.current.map(ref => {
+      // Get the y-coordinate of each user div. rect is for rectangle
+      const userYCoordinates = userRefs.current.map((ref) => {
         const rect = ref.getBoundingClientRect();
         return rect.top;
       });
       // Find the index of the user div closest to the top border, thats the user we select
-      const closestIndex = userYCoordinates.indexOf(Math.min(...userYCoordinates));
+      const closestIndex = userYCoordinates.indexOf(
+        Math.min(...userYCoordinates),
+      );
       const user = users[closestIndex];
 
-      setSelectedUser(user.name);
-      // onUserSelected(user);
+      setSelectedUser(user);
 
       //Cynthia addition
-      const shouldChat = window.confirm(`Do you want to chat with ${user.name}?`);
+      const shouldChat = window.confirm(
+        `Do you want to chat with ${user.name}?`,
+      );
       if (shouldChat) {
         setChatStarted(true);
       }
-
     }, rotationDuration);
   };
 
   return (
     <div style={{ width: '50%', margin: 'auto', textAlign: 'center' }}>
+      <h3>Have Fate Pick your Date</h3>
       <div
         style={{
           // Wheel Container
@@ -108,13 +109,13 @@ const Wheel = ({ user }) => {
             // Calculate the rotation angle of each user div
             const userRotationAngle = index * (360 / users.length);
 
-            // Calculate the translation values to move the user div up
-            const radius = 200; // Adjust this value to control the distance from the center
+            // Calculate the translation values to move the user divs vertically.
+            const radius = 200; // Adjust this value to control the distance of names from the center of wheel.
             const translationY = -radius;
 
             return (
               <div
-                ref={ref => (userRefs.current[index] = ref)}
+                ref={(ref) => (userRefs.current[index] = ref)}
                 key={index}
                 style={{
                   position: 'absolute',
@@ -132,17 +133,35 @@ const Wheel = ({ user }) => {
             );
           })}
         </div>
+        <div
+          style={{
+            // Checkboxes container
+            position: 'absolute',
+            bottom: '0',
+            left: '5px',
+            padding: '0px',
+            display: 'flex',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            color: 'white',
+            textShadow: 'black 0px 0px 2px',
+            fontSize: '10px',
+          }}
+        >
+          <FormControlLabel control={<Checkbox />} label='girls' />
+          <FormControlLabel control={<Checkbox />} label='boys' />
+        </div>
       </div>
-      <h3>Have Fate Pick your Date</h3>
+
       {selectedUser ? (
         <div>
-          <button onClick={spinWheel}>Spin Again</button>
-          <p>Selected User: {selectedUser}</p>
-          { chatStarted && <Chat initialUser={ user.name } />}
+          <Button variant="contained" onClick={spinWheel}>Spin Again</Button>
+          <p>Selected User: {selectedUser.name}</p>
+          {chatStarted && <Chat initialUser={user} selectedUser={selectedUser} />}
         </div>
       ) : (
         <div>
-          <button onClick={spinWheel}>Spin the Wheel</button>
+          <Button variant="contained" onClick={spinWheel}>Spin the Wheel</Button>
         </div>
       )}
     </div>
