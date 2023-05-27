c3205f15130116c70f3a33d85fd48ce47308d0ac
logan <loganh42@gmail.com>
2023-05-26 21:03:18 -0500

    New Login Screen Style

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Typography, Box } from '@mui/material';
+import { Button, Typography, Box, Grid, Avatar, Paper } from '@mui/material';
 
 
 const Login = ({ login }) => {
@@ -7,13 +7,77 @@ const Login = ({ login }) => {
   //The login function is from the App.jsx and changes the state there. Using the material UI Button
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ textAlign: 'center' }}>
-        <Typography variant="h2" gutterBottom>WHEEL OF LOVE</Typography>
-        <Button variant="contained" size="medium" onClick={login} sx={{ backgroundColor: '#926aa6' }}>Login with Google</Button>
-      </div>
-    </Box>
+    <div
+      style={{
+        height: '100vh',
+        backgroundImage: 'url(https://i.imgur.com/rxlrajy_d.webp?maxwidth=760&fidelity=grand)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Grid
+        container
+        component="main"
+        sx={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            p: 4,
+            border: '10px solid #6a7ea6',
+            boxShadow: '0 0 0 4px #140e17',
+            borderRadius: '50%',
+            //Get rid of these if you want the box back:
+            width: '50vh',
+            height: '50vh',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box sx={{ mt: 1 }}>
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, backgroundColor: '#926aa6' }}
+                onClick={ login }
+              >
+                Login with Google
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
   );
+
 
 };
 
