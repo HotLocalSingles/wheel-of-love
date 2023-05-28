cee6c2ba63e84478d9714d702083c3220530e0f4
logan <loganh42@gmail.com>
2023-05-27 19:32:38 -0500

    New Login Page, other Components Inheriting

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 
 import '../../../styles/login.css';
+import '@fontsource/sacramento';
 
 
 const Login = ({ login }) => {
@@ -13,45 +14,60 @@ const Login = ({ login }) => {
   //The login function is from the App.jsx and changes the state there. Using the material UI Button
 
   return (
-    <div className="login-container">
-      <Grid
-        container
-        component="main"
-        className="login-grid"
-      >
-        <Grid
-          item
-          xs={12}
-          sm={8}
-          md={5}
-          component={Paper}
-          elevation={6}
-          square
-          className="login-paper"
-        >
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography component="h1" variant="h5">Sign in</Typography>
-            <Box sx={{ mt: 1 }}>
-              <Button
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, backgroundColor: '#926aa6' }}
-                onClick={login}
-              >
-                Login with Google
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
+    <Grid container spacing={3}>
+      <Grid item xs={12} alignItems="center" style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <div class="neonText" onClick={login} style={{ cursor: 'pointer' }}>
+          <Typography variant="h1" fontFamily="Sacramento">Wheel of Love</Typography>
+        </div>
       </Grid>
-    </div>
+    </Grid>
+
   );
+  // return (
+  //   <div className="login-container">
+  //     <Grid
+  //       container
+  //       component="main"
+  //       className="login-grid"
+  //     >
+  //       <Grid
+  //         item
+  //         xs={12}
+  //         sm={8}
+  //         md={5}
+  //         component={Paper}
+  //         elevation={6}
+  //         square
+  //         className="login-paper"
+  //       >
+  // <Box
+  //   sx={{
+  //     display: 'flex',
+  //     flexDirection: 'column',
+  //     alignItems: 'center',
+  //   }}
+  // >
+  //   <Typography component="h1" variant="h5">Sign in</Typography>
+  //   <Box sx={{ mt: 1 }}>
+  //     <Button
+  //       fullWidth
+  //       variant="contained"
+  //       sx={{ mt: 3, mb: 2, backgroundColor: '#926aa6' }}
+  //       onClick={login}
+  //     >
+  //       Login with Google
+  //     </Button>
+  //   </Box>
+  // </Box>
+  //       </Grid>
+  //     </Grid>
+  //   </div>
+  // );
 };
 
 export default Login;
