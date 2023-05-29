23a852e93d25f57810d007ecf83c0953dd1e7489
logan <loganh42@gmail.com>
2023-05-29 02:23:52 -0500

    Fix Match Server Request

--- server/routes/matchData.js
+++ server/routes/matchData.js
@@ -6,12 +6,12 @@ const { Op } = require('sequelize');
 const { User, Match } = require('../db/models');
 
 //Get matches for a specific user
-router.get('/:userId', async (req, res) => {
+router.get('/matches/:userId', async (req, res) => {
   try {
     const { userId } = req.params;
-    console.log(userId);
+    console.log(userId)
     const user = await User.findByPk(userId);
-    console.log(user);
+    console.log(user)
     if (!user) {
       return res.sendStatus(404);
     }
